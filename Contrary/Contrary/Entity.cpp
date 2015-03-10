5095612fa9ed372aa0b62759dd26bb0136f6b484
MgBag <waanders.b@gmail.com>
2015-03-10 09:07:12 +0100

    Did the entity class

--- Contrary/Contrary/Entity.cpp
+++ Contrary/Contrary/Entity.cpp
@@ -1,11 +1,136 @@
 #include "Entity.h"
 
+Entity::Entity(Model model, ALLEGRO_COLOR color, double x, double y, double mass = 10.0, double friction = 1.0, double bounciness = 0.0, double rotation = 0.0)
+{
+	m_model = model;
+	m_color = color;
+	m_position = Coordinates(x, y);
+	m_mass = mass;
+	m_friction = friction;
+	m_bounciness = bounciness;
+	m_rotation = rotation;
+
+	m_centerOfMass = Coordinates(0.0, 0.0);
+	m_velocity = Velocity(0.0, 0.0);
+	m_angularVelocity = 0.0;
+}
 
 Entity::Entity()
 {
+	// TODO : Make standard model function;
+	// m_model = ;
+	m_color = al_map_rgb(220, 220, 220);
+	m_position = Coordinates(0.0, 0.0);
+	m_mass = 10.0;
+	m_friction = 1.0;
+	m_bounciness = 0.0;
+	m_rotation = 0.0;
+	m_centerOfMass = Coordinates(0.0, 0.0);
+	m_velocity = Velocity(0.0, 0.0);
+	m_angularVelocity = 0.0;
 }
 
 
 Entity::~Entity()
 {
+}
+
+void Entity::SetModel(Model model)
+{
+	m_model = model;
+}
+
+void Entity::SetColor(ALLEGRO_COLOR color)
+{
+	m_color = color;
+}
+
+void Entity::SetCenterOfMass(Coordinates coordinates)
+{
+	m_centerOfMass = coordinates;
+}
+
+void Entity::SetPosition(Coordinates position)
+{
+	m_position = position;
+}
+
+void Entity::SetVelocity(Velocity velocity)
+{
+	m_velocity = velocity;
+}
+
+void Entity::SetMass(double mass)
+{
+	m_mass = mass;
+}
+
+void Entity::SetFriction(double friction)
+{
+	m_friction = friction;
+}
+
+void Entity::SetBounciness(double bounciness)
+{
+	m_bounciness = bounciness;
+}
+
+void Entity::SetRotation(double rotation)
+{
+	m_rotation = rotation;
+}
+
+void Entity::SetAngularVelocity(double angularVelocity)
+{
+	m_angularVelocity = angularVelocity;
+}
+
+Model* Entity::GetModel()
+{
+	return &m_model;
+}
+
+ALLEGRO_COLOR Entity::Color()
+{
+	return m_color;
+}
+
+Coordinates* Entity::CenterOfMass()
+{
+	return &m_centerOfMass;
+}
+
+Coordinates* Entity::Position()
+{
+	return &m_position;
+}
+
+Velocity* Entity::GetVelocity()
+{
+	return &m_velocity;
+}
+
+double Entity::Mass()
+{
+	return m_mass;
+}
+
+double Entity::Friction()
+{
+	return m_friction;
+}
+
+double Entity::Bounciness()
+{
+	return m_bounciness;
+}
+
+double Entity::Rotation()
+{
+	return m_rotation;
+}
+
+double Entity::AngularVelocity()
+{
+	return m_angularVelocity;
 }
--- Contrary/Contrary/Entity.h
+++ Contrary/Contrary/Entity.h
@@ -8,43 +8,46 @@ using namespace std;
 
 class Entity
 {
+private:
+	Model m_model;
+	Coordinates m_centerOfMass;
+	ALLEGRO_COLOR m_color;
+	Coordinates m_position;
+	Velocity m_velocity;
+	double m_mass;
+	double m_friction;
+	double m_bounciness;
+	double m_rotation;
+	// In r/s
+	double m_angularVelocity;
+
 public:
 	Entity(Model model, ALLEGRO_COLOR color, double x, double y, double mass = 10.0, double friction = 1.0, double bounciness = 0.0, double rotation = 0.0);
 	Entity();
 	~Entity();
 
-	Model* GetModel();
-	ALLEGRO_COLOR Color();
-	Coordinates* CenterOfMass();
-	double Mass();
-	double Friction();
-	double Bounciness();
-	double Rotation();
-	Coordinates* Position();
-	Velocity* GetVelocity();
-	double AngularVelocity();
-
 	void SetModel(Model model);
 	void SetColor(ALLEGRO_COLOR color);
 	void SetCenterOfMass(Coordinates coordinates);
+	void SetPosition(Coordinates position);
+	void SetVelocity(Velocity velocity);
 	void SetMass(double mass);
 	void SetFriction(double friction);
 	void SetBounciness(double bounciness);
 	void SetRotation(double rotation);
-	void SetPosition(Coordinates position);
-	void SetVelocity(Velocity velocity);
 	void SetAngularVelocity(double angularVelocity);
 
-private:
-	Model m_model;
-	ALLEGRO_COLOR m_color;
-	Coordinates m_centerOfMass;
-	double m_mass;
-	double m_friction;
-	double m_bounciness;
-	double m_rotation;
-	Coordinates m_position;
-	Velocity m_velocity;
-	double m_angularVelocity;
+	// Naming this Model breaks because model is also a class.
+	Model* GetModel();
+	ALLEGRO_COLOR Color();
+	Coordinates* CenterOfMass();
+	Coordinates* Position();
+	// Naming this Velocity breaks because Velocity is also a class.
+	Velocity* GetVelocity();
+	double Mass();
+	double Friction();
+	double Bounciness();
+	double Rotation();
+	double AngularVelocity();
 };
 
